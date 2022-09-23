084702113a8fdc434a2fd50e27412895907433fd
Ibrahim Ahmad <54756819+IamIbrahimAhmad@users.noreply.github.com>
2022-09-23 16:13:39 -0400

    pokemon

---
+++ js/app.js
@@ -0,0 +1,29 @@
+function setup(){
+    createCanvas(1400, 750);
+}
+
+function draw() {
+    background(100);
+    fill("red");
+    arc(650, 350, 280, 265, 15.7, PI + PI);
+    fill("black");
+    rect(525,351,250,25)
+    fill("white");
+    arc(650, 375, 280, 265, 0, 15.71, PIE);
+    fill("black");
+    circle(655, 363, 75)
+    fill("white");
+    circle(655, 363, 50)
+}
+
+/*function mousePressed(){
+    if(color === "blue"){
+        color = "green";
+        fill(color);
+        circle(750, 375, 100)
+    }else{
+        color = "blue";
+        fill(color);
+        circle(750, 375, 100)   
+    }
+}*/
